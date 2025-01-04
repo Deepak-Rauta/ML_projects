6ab3ffb2c764faead0d22c00db9cb12f2b2a76fd
Deepak kumar Rauta <kdeepak8250@gmail.com>
2025-01-04 15:11:36 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+from src.logger import logging
+
 
 def error_message_details(error, error_detail:sys): # when error occurs i raise my own custome message i.e error, error_detail was present inside the sys
     _,_,exc_tb = error_detail.exc_info()
@@ -8,8 +10,6 @@ def error_message_details(error, error_detail:sys): # when error occurs i raise 
 
     return error_message
 
-    
-
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
@@ -17,6 +17,16 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+if __name__ == '__main__':
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Zero division Error!")
+        raise CustomException(e, sys)
+
+
+
 
 
 
